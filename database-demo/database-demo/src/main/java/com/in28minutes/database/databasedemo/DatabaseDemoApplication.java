91dc190611c777ab5fbe1821921ba536de1dcb51
nishant bodade <nishantbodade@gmail.com>
2022-05-01 12:38:16 +0530

    implement inser and update using spring jdbc

--- database-demo/database-demo/src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ database-demo/database-demo/src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -3,6 +3,8 @@ package com.in28minutes.database.databasedemo;
 
 
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.in28minutes.database.databasedemo.entity.Person;
 import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDao;
 
 @SpringBootApplication
@@ -29,6 +32,13 @@ public class DatabaseDemoApplication  implements CommandLineRunner{
 		logger.info("all data => {}",dao.findAll());
 		logger.info("user id data data => {}",dao.findById(10001));
 		logger.info("user id data delete => {}",dao.deleteById(10001));
+
+		logger.info("Inserting 10004 -> {}", 
+				dao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+		
+		logger.info("Update 10003 -> {}", 
+				dao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+		
 		
 	}
 
