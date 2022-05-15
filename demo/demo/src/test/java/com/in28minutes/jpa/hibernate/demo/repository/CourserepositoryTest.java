0651bb7dee373749deb516947f978fcaefc78f7a
nishant bodade <nishantbodade@gmail.com>
2022-05-15 17:06:27 +0530

    ManyToOne Mapping - Wrapping up

--- demo/demo/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourserepositoryTest.java
+++ demo/demo/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourserepositoryTest.java
@@ -3,22 +3,32 @@ package com.in28minutes.jpa.hibernate.demo.repository;
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.in28minutes.jpa.hibernate.demo.DemoApplication;
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Review;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DemoApplication.class )
 class CourserepositoryTest {
+	Logger logger=LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	CourseRepository repository;
+	
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void findById_basic() {
@@ -63,5 +73,20 @@ class CourserepositoryTest {
 	public void playWithEntityManager() {
 		repository.playWithEntityManager();
 	}
+	
+	@Test
+	@Transactional
+	public void retriveReviewForCorse() {
+		Course course=repository.findById(10001L);
+		logger.info("{}",course.getReviews());
+	}
+	
+	@Test
+	@Transactional
+	public void retriveCourseForReview() {
+		Review review=em.find(Review.class, 50001L);
+		logger.info("{}",review.getCourse());
+	}
+
 
 }
